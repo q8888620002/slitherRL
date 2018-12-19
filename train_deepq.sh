d5f42ee5f4705afaa49b48d8ac405ac9526eda9f
MIngyulu <q8888620002@users.noreply.github.com>
2018-12-18 20:43:34 -0500

    Delete train_deepq.sh

--- train_deepq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export OPENAI_REMOTE_VERBOSE=0
-ps -ef | grep tensorboard | grep -v grep | awk '{print $2}' | xargs kill
-cd agent
-rm -r nohup.out
-rm -r results/deep_q/events.out.tfevents*
-rm -r results/deep_q/log.txt
-tensorboard --logdir=results/deep_q/ &
-nohup python run.py -mod q&

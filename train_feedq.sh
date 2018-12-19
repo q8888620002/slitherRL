f7e0fa47f0766e3b77b4d5c7f926905ccc7f5cae
MIngyulu <q8888620002@users.noreply.github.com>
2018-12-18 20:43:42 -0500

    Delete train_feedq.sh

--- train_feedq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export OPENAI_REMOTE_VERBOSE=0
-ps -ef | grep tensorboard | grep -v grep | awk '{print $2}' | xargs kill
-cd agent
-rm -r nohup.out
-rm -r results/feedforward_q/events.out.tfevents*
-rm -r results/feedforward_q/log.txt
-tensorboard --logdir=results/feedforward_q/ &
-nohup python run.py -net feedforward_q -mod q -typ features&
